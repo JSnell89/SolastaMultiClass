e976b4e4028de82cd4fe025c097b5b907e277834
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-01 23:56:16 -0700

    .

--- SolastaMultiClass/Viewers/HelpViewer.cs
+++ SolastaMultiClass/Viewers/HelpViewer.cs
@@ -18,8 +18,6 @@ namespace SolastaMultiClass.Viewers
                     UI.Label("Multi Class (BETA VERSION)".yellow().bold());
                     UI.Div();
                     UI.Label("Current limitations:".yellow());
-                    UI.Label(". don't multiclass into a cleric or paladin without a deity...");
-                    UI.Label(". so, for now, deity selection is forced on all new characters");
                     UI.Label(". need to rework the spell system for multi-class (partially done - Current progress of shared spellcasting can be turned off but it's not recommended)");
                     UI.Label(". Note for spellcasting - Inspecting the character will not show things properly out of a game and characters leveled up in the pool need a long in game rest to have the proper spell slots refreshed)");
                     UI.Label(". need to rework channel divinity, unarmored defense and other multi-class rules");
