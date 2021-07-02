81fb1ae9bf9830caec0218b4422a8170c964e674
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-01 20:14:42 -0700

    tweak help

--- SolastaMultiClass/Viewers/HelpViewer.cs
+++ SolastaMultiClass/Viewers/HelpViewer.cs
@@ -21,8 +21,9 @@ namespace SolastaMultiClass.Viewers
                     UI.Label(". don't multiclass into a cleric or paladin without a deity...");
                     UI.Label(". so, for now, deity selection is forced on all new characters");
                     UI.Label(". need to rework the spell system for multi-class (partially done - Current progress of shared spellcasting can be turned off but it's not recommended)");
-                    UI.Label(". Note for spellcasting - Inspecting the character will not show things properly out of a game and characters leveled up in the character builder need a long rest in game to have proper spell slots get refreshed.)");
+                    UI.Label(". Note for spellcasting - Inspecting the character will not show things properly out of a game and characters leveled up in the pool need a long in game rest to have the proper spell slots refreshed)");
                     UI.Label(". need to rework channel divinity, unarmored defense and other multi-class rules");
+                    UI.Label(". need to correctly filter fighting styles on inspection panel");
                     UI.Label("");
                     UI.Label("Inspection Screen Instructions:".yellow());
                     UI.Label(". press LEFT / RIGHT arrows in the character inspection tab to browser for other classes features...");
