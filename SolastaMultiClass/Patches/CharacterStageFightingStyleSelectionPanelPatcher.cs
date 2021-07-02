cb499323fd1e1874004451d8bc46911cd22e8ada
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-02 10:48:20 -0700

    Transpilers is in the air

--- SolastaMultiClass/Patches/CharacterStageFightingStyleSelectionPanelPatcher.cs
+++ SolastaMultiClass/Patches/CharacterStageFightingStyleSelectionPanelPatcher.cs
@@ -1,25 +1,18 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Reflection.Emit;
 
 namespace SolastaMultiClass.Patches
 {
     internal static class CharacterStageFightingStyleSelectionPanelPatcher
     {
-        internal static int previouslySelectedFightingStyle = -1;
+        public static int previouslySelectedFightingStyle = -1;
 
-        internal static void UntrainPreviouslySelectedFightStyle(CharacterStageFightingStyleSelectionPanel __instance, FightingStyleDefinition fightingStyleDefinition)
+        public static void UntrainPreviouslySelectedFightStyle(ICharacterBuildingService __instance)
         {
-            var x = 0;
+            __instance.UntrainLastFightingStyle();
         }
-        
-        internal static void RefreshNow(CharacterStageFightingStyleSelectionPanel __instance)
-        {
-            __instance.CommonData.AbilityScoresListingPanel.RefreshNow();
-            __instance.CommonData.CharacterStatsPanel.RefreshNow();
-        }
-        // OnBeginShow
-
+       
         [HarmonyPatch(typeof(CharacterStageFightingStyleSelectionPanel), "OnBeginShow")]
         internal static class CharacterStageFightingStyleSelectionPanel_OnBeginShow_Patch
         {
@@ -32,57 +25,45 @@ namespace SolastaMultiClass.Patches
         [HarmonyPatch(typeof(CharacterStageFightingStyleSelectionPanel), "Refresh")]
         internal static class CharacterStageFightingStyleSelectionPanel_Refresh_Patch
         {
-            internal static bool Prefix(CharacterStageFightingStyleSelectionPanel __instance,
-                                        List<FightingStyleDefinition> ___compatibleFightingStyles,
-                                        RectTransform ___fightingStylesTable,
-                                        RectTransform ___selectedFightingStyleGroup,
-                                        int ___selectedFightingStyle,
-                                        GuiLabel ___selectedFightingStyleTitle,
-                                        GuiLabel ___fightingStyleDescription)
+            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                for (int index = 0; index < ___compatibleFightingStyles.Count; ++index)
-                    ___fightingStylesTable.GetChild(index).GetComponent<DiskSelectionSlot>().RefreshState(___selectedFightingStyle == index, true);
-                if (___selectedFightingStyle >= 0)
+                var untrainLastFightingStyleMethod = typeof(ICharacterBuildingService).GetMethod("UntrainLastFightingStyle");
+                var untrainPreviouslySelectedFightStyleMethod = typeof(CharacterStageFightingStyleSelectionPanelPatcher).GetMethod("UntrainPreviouslySelectedFightStyle");
+
+                foreach (var instruction in instructions)
                 {
-                    ___selectedFightingStyleGroup.gameObject.SetActive(true);
-                    ___selectedFightingStyleTitle.Text = ___compatibleFightingStyles[___selectedFightingStyle].GuiPresentation.Title;
-                    FightingStyleDefinition compatibleFightingStyle = ___compatibleFightingStyles[___selectedFightingStyle];
-                    //__instance.CharacterBuildingService.UntrainLastFightingStyle();
-                    if (previouslySelectedFightingStyle >= 0)
+                    if (instruction.Calls(untrainLastFightingStyleMethod))
                     {
-                        UntrainPreviouslySelectedFightStyle(__instance, ___compatibleFightingStyles[previouslySelectedFightingStyle]);
+                        yield return new CodeInstruction(OpCodes.Call, untrainPreviouslySelectedFightStyleMethod);
                     }
-                    __instance.CharacterBuildingService.UntrainLastFightingStyle();
-                    __instance.CharacterBuildingService.TrainFightingStyle(compatibleFightingStyle);
-                    ___fightingStyleDescription.Text = compatibleFightingStyle.FormatDescription();
-                    RefreshNow(__instance);
+                    else
+                    {
+                        yield return instruction;
+                    }
                 }
-                else
-                {
-                    ___selectedFightingStyleGroup.gameObject.SetActive(false);
-                }
-                return false;
             }
         }
 
         [HarmonyPatch(typeof(CharacterStageFightingStyleSelectionPanel), "OnFightingStyleValueChangedCb")]
         internal static class CharacterStageFightingStyleSelectionPanel_OnFightingStyleValueChangedCb_Patch
         {
-            internal static bool Prefix(CharacterStageFightingStyleSelectionPanel __instance,
-                                        RectTransform ___fightingStylesTable,
-                                        ref int ___selectedFightingStyle,
-                                        BaseSelectionSlot selectedSlot)
+            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                for (int index = 0; index < ___fightingStylesTable.childCount; ++index)
+                var previouslySelectedFightingStyleField = AccessTools.Field(typeof(CharacterStageFightingStyleSelectionPanelPatcher), "previouslySelectedFightingStyle");
+
+                foreach (var instruction in instructions)
                 {
-                    if (___fightingStylesTable.GetChild(index).GetComponent<BaseSelectionSlot>() == (Object)selectedSlot)
+                    if (instruction.StoresField(AccessTools.Field(typeof(CharacterStageFightingStyleSelectionPanel), "selectedFightingStyle")))
                     {
-                        previouslySelectedFightingStyle = ___selectedFightingStyle;
-                        ___selectedFightingStyle = index;
-                        RefreshNow(__instance);
+                        yield return instruction;
+                        yield return new CodeInstruction(OpCodes.Ldloc_0);
+                        yield return new CodeInstruction(OpCodes.Stsfld, previouslySelectedFightingStyleField);
+                    }
+                    else
+                    {
+                        yield return instruction;
                     }
                 }
-                return false;
             }
         }
     }
