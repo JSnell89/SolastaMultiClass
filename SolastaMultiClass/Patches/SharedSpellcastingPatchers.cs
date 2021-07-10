adb1202b76f03566a2d767466005c351244e9ccb
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-09 20:08:46 -0700

    should consider building service first on GetHero

--- SolastaMultiClass/Patches/SharedSpellcastingPatchers.cs
+++ SolastaMultiClass/Patches/SharedSpellcastingPatchers.cs
@@ -213,7 +213,7 @@ namespace SolastaMultiClass.Patches
                 for (int i = 0; i < maxSpellLevel; i++)
                 {
                     currentInstanceSpellsSlotCapacities[i + 1] = FullCastingSlots[casterLevel - 1].Slots[i]; //The real change right here
-                    
+
                     // I believe this is just to properly handle saves between patches, theoretically it is needed for higher level slots for MC saves between patches as well
                     if (legacyAvailableSpellsSlots.ContainsKey(i + 1))
                     {
@@ -557,60 +557,63 @@ namespace SolastaMultiClass.Patches
         //TODO add Bard and other potential full casters - Note Warlock should not be included handle everything on their own since pact/short rest spell slots will not be shared in any fashion with other classes.
         private static readonly CharacterClassDefinition[] FullCasterList = new CharacterClassDefinition[]
         {
-                    DatabaseHelper.CharacterClassDefinitions.Cleric,
-                    DatabaseHelper.CharacterClassDefinitions.Wizard,
+                     DatabaseHelper.CharacterClassDefinitions.Cleric,
+                     DatabaseHelper.CharacterClassDefinitions.Wizard,
         };
 
         private static readonly CharacterClassDefinition[] HalfCasterList = new CharacterClassDefinition[]
         {
-                    DatabaseHelper.CharacterClassDefinitions.Paladin,
-                    DatabaseHelper.CharacterClassDefinitions.Ranger,
+                     DatabaseHelper.CharacterClassDefinitions.Paladin,
+                     DatabaseHelper.CharacterClassDefinitions.Ranger,
         };
 
         private static readonly CharacterSubclassDefinition[] OneThirdCasterList = new CharacterSubclassDefinition[]
         {
-                    DatabaseHelper.CharacterSubclassDefinitions.MartialSpellblade,
-                    DatabaseHelper.CharacterSubclassDefinitions.RoguishShadowCaster,
+                     DatabaseHelper.CharacterSubclassDefinitions.MartialSpellblade,
+                     DatabaseHelper.CharacterSubclassDefinitions.RoguishShadowCaster,
         };
 
         private static readonly List<SlotsByLevelDuplet> FullCastingSlots = new List<SlotsByLevelDuplet>()
-                {
-                    //Add 10th level slots that are always 0 since Solasta seems to rely on IndexOf(0) for certain things
-                    new SlotsByLevelDuplet() { Slots = new List<int> {2,0,0,0,0,0,0,0,0,0}, Level = 1 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {3,0,0,0,0,0,0,0,0,0}, Level = 2 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,2,0,0,0,0,0,0,0,0}, Level = 3 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,0,0,0,0,0,0,0,0}, Level = 4 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,2,0,0,0,0,0,0,0}, Level = 5 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,0,0,0,0,0,0,0}, Level = 6 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,1,0,0,0,0,0,0}, Level = 7 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,2,0,0,0,0,0,0}, Level = 8 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,1,0,0,0,0,0}, Level = 9 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,0,0,0,0,0}, Level = 10 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,0,0,0,0}, Level = 11 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,0,0,0,0}, Level = 12 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,0,0,0}, Level = 13 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,0,0,0}, Level = 14 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,1,0,0}, Level = 15 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,1,0,0}, Level = 16 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,1,1,0}, Level = 17 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,3,1,1,1,1,0}, Level = 18 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,3,2,1,1,1,0}, Level = 19 },
-                    new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,3,2,2,1,1,0}, Level = 20 },
-                };
+                 {
+                     //Add 10th level slots that are always 0 since Solasta seems to rely on IndexOf(0) for certain things
+                     new SlotsByLevelDuplet() { Slots = new List<int> {2,0,0,0,0,0,0,0,0,0}, Level = 1 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {3,0,0,0,0,0,0,0,0,0}, Level = 2 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,2,0,0,0,0,0,0,0,0}, Level = 3 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,0,0,0,0,0,0,0,0}, Level = 4 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,2,0,0,0,0,0,0,0}, Level = 5 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,0,0,0,0,0,0,0}, Level = 6 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,1,0,0,0,0,0,0}, Level = 7 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,2,0,0,0,0,0,0}, Level = 8 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,1,0,0,0,0,0}, Level = 9 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,0,0,0,0,0}, Level = 10 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,0,0,0,0}, Level = 11 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,0,0,0,0}, Level = 12 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,0,0,0}, Level = 13 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,0,0,0}, Level = 14 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,1,0,0}, Level = 15 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,1,0,0}, Level = 16 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,2,1,1,1,1,0}, Level = 17 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,3,1,1,1,1,0}, Level = 18 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,3,2,1,1,1,0}, Level = 19 },
+                     new SlotsByLevelDuplet() { Slots = new List<int> {4,3,3,3,3,2,2,1,1,0}, Level = 20 },
+                 };
 
         private static RulesetCharacterHero GetHero(string name)
         {
-            var gameService = ServiceRepository.GetService<IGameService>();
+            
             var characterBuildingService = ServiceRepository.GetService<ICharacterBuildingService>();
 
-            if (gameService?.Game != null)
+            if (characterBuildingService?.HeroCharacter != null)
             {
-                return (RulesetCharacterHero) gameService.Game.GameCampaign.Party.CharactersList.Find(x => x.RulesetCharacter.Name == name).RulesetCharacter;
+                return characterBuildingService.HeroCharacter;
             }
+            else
+            {
+                var gameService = ServiceRepository.GetService<IGameService>();
+                var gameCampaignCharacter = gameService?.Game?.GameCampaign?.Party?.CharactersList.Find(x => x.RulesetCharacter.Name == name);
 
-            return characterBuildingService?.HeroCharacter;
+                return (RulesetCharacterHero)gameCampaignCharacter?.RulesetCharacter;
+            }
         }
-
-        internal static List<RulesetCharacterHero> HeroesPool = new List<RulesetCharacterHero>();
     }
 }
