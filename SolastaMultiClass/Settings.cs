45adda79b72b84d9105af9f74ed4459bbf6165ee
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-10 19:46:59 -0700

    ideas on how to support other classes

--- SolastaMultiClass/Settings.cs
+++ SolastaMultiClass/Settings.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityModManagerNet;
 
 namespace SolastaMultiClass
@@ -14,6 +15,20 @@ namespace SolastaMultiClass
         public bool EnableSharedSpellCasting = true;
         //public bool TurnOffSpellPreparationRestrictions = false;
         public bool EnableNonStackingExtraAttacks = true;
+
+        public Dictionary<string, string> ClassCasterType = new Dictionary<string, string>()
+        {
+            { "Bard", "Full" },
+            { "BardClass", "Full" },
+            { "Cleric", "Full" },
+            { "Sorcerer", "Full" },
+            { "Wizard", "Full" },
+            { "Paladin", "Half" },
+            { "Ranger", "Half" },
+            { "ClassTinkerer", "HalfCeiling" },
+            { "MartialSpellblade", "OneThird" },
+            { "RoguishShadowCaster", "OneThird" }
+        };
 
         public const InputCommands.Id PLAIN_LEFT = (InputCommands.Id)22220003;
         public const InputCommands.Id PLAIN_RIGHT = (InputCommands.Id)22220004;
