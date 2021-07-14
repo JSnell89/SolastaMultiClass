027a273fd3e814fb385e2f7e4345b4b5898a040f
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-13 20:48:07 -0700

    clean up code

--- SolastaMultiClass/Patches/CharacterInformationPanelPatcher.cs
+++ SolastaMultiClass/Patches/CharacterInformationPanelPatcher.cs
@@ -15,18 +15,18 @@ namespace SolastaMultiClass.Patches
             internal static bool Prefix(CharacterInformationPanel __instance, List<BaseDefinition> ___badgeDefinitions, RectTransform ___classBadgesTable, GameObject ___classBadgePrefab)
             {
                 ___badgeDefinitions.Clear();
-                RulesetCharacterHero rulesetCharacterHero = __instance.InspectedCharacter.RulesetCharacterHero;
+                var rulesetCharacterHero = __instance.InspectedCharacter.RulesetCharacterHero;
                 foreach (KeyValuePair<CharacterClassDefinition, CharacterSubclassDefinition> classesAndSubclass in rulesetCharacterHero.ClassesAndSubclasses)
                     if (classesAndSubclass.Key == GameUi.GetSelectedClass())
-                        ___badgeDefinitions.Add((BaseDefinition)classesAndSubclass.Value);
-                if ((BaseDefinition)rulesetCharacterHero.DeityDefinition != (BaseDefinition)null && (GameUi.GetSelectedClass() == Paladin || GameUi.GetSelectedClass() == Cleric))
-                    ___badgeDefinitions.Add((BaseDefinition)rulesetCharacterHero.DeityDefinition);
-                foreach (BaseDefinition trainedFightingStyle in GameUi.GetTrainedFightingStyles(rulesetCharacterHero))
+                        ___badgeDefinitions.Add(classesAndSubclass.Value);
+                if (rulesetCharacterHero.DeityDefinition != null && (GameUi.GetSelectedClass() == Paladin || GameUi.GetSelectedClass() == Cleric))
+                    ___badgeDefinitions.Add(rulesetCharacterHero.DeityDefinition);
+                foreach (var trainedFightingStyle in GameUi.GetTrainedFightingStyles(rulesetCharacterHero))
                     ___badgeDefinitions.Add(trainedFightingStyle);
                 while (___classBadgesTable.childCount < ___badgeDefinitions.Count)
                     Gui.GetPrefabFromPool(___classBadgePrefab, (Transform)___classBadgesTable);
                 int index = 0;
-                foreach (BaseDefinition badgeDefinition in ___badgeDefinitions)
+                foreach (var badgeDefinition in ___badgeDefinitions)
                 {
                     Transform child = ___classBadgesTable.GetChild(index);
                     child.gameObject.SetActive(true);
