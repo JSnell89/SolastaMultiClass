881dc68fe40a02928f0f212aaf500fc79402e02f
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-18 07:04:17 -0700

    rollback to Postfix

--- SolastaMultiClass/Patches/GuiCharacterPatcher.cs
+++ SolastaMultiClass/Patches/GuiCharacterPatcher.cs
@@ -7,30 +7,27 @@ namespace SolastaMultiClass.Patches
         [HarmonyPatch(typeof(GuiCharacter), "MainClassDefinition", MethodType.Getter)]
         internal static class GuiCharacter_MainClassDefinition_Patch
         {
-            internal static bool Prefix(ref CharacterClassDefinition __result)
+            internal static void Postfix(ref CharacterClassDefinition __result)
             {
                 __result = Models.InspectionPanelContext.GetSelectedClass( __result);
-                return false;
             }
         }
 
         [HarmonyPatch(typeof(GuiCharacter), "LevelAndClassAndSubclass", MethodType.Getter)]
         internal static class GuiCharacter_LevelAndClassAndSubclass_Patch
         {
-            internal static bool Postfix(GuiCharacter __instance, ref string __result)
+            internal static void Postfix(GuiCharacter __instance, ref string __result)
             {
                 __result = Models.GameUi.GetAllClassesLabel(__instance, __result, " - ");
-                return false;
             }
         }
 
         [HarmonyPatch(typeof(GuiCharacter), "ClassAndLevel", MethodType.Getter)]
         internal static class GuiCharacter_ClassAndLevel_Patch
         {
-            internal static bool Postfix(GuiCharacter __instance, ref string __result)
+            internal static void Postfix(GuiCharacter __instance, ref string __result)
             {
                 __result = Models.GameUi.GetAllClassesLabel(__instance, __result, " - ");
-                return false;
             }
         }
     }
